676e57f2c0a9f6a985bb5d73afe54559e2c6481f
SarvasiddiRamya <72144168+SarvasiddiRamya@users.noreply.github.com>
2020-11-28 17:43:46 +0530

    Add files via upload

---
+++ Online Wallet/Home1.cs
@@ -0,0 +1,25 @@
+@{
+    ViewBag.Title = "Home1";
+}
+<h2 style="text-align:center;color:darkblue">Online Wallet System</h2>
+
+@*<h2 align="center">Payment Wallet System</h2>*@
+
+@*<div class="form-group" align="center">
+
+    <a href="@Url.Action("Add", "HomePage")" class="btn btn-default">Add Money</a>
+
+</div>
+
+<div class="form-group" align="center">
+
+    <a href="@Url.Action("CheckBalance", "HomePage")" class="btn btn-default">Check Balance</a>
+
+
+</div>
+<div class="form-group" align="center">
+
+    <a href="@Url.Action("Transaction", "HomePage")" class="btn btn-default">Transfer Money</a>
+
+
+</div>*@
